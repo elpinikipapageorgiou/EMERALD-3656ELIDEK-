b29a5f7cd3ef74ce441fdc7fb521b094cf2a91df
Anna Feleki <felekiann@gmail.com>
2025-01-31 16:33:28 +0200

    Add files via upload

---
+++ Clinical/FCM-Driven/FCM-ELM/scripts/suggested_weights_calculation.py
@@ -0,0 +1,54 @@
+import random
+import numpy as np
+
+
+def calculate_initial_weights(num_dimensions, initial_weight_matrix=None):
+    if initial_weight_matrix is not None:
+        initial_weight_matrix = initial_weight_matrix.to_numpy()
+        for i, row in enumerate(initial_weight_matrix):
+            for j, val in enumerate(row):
+
+                if initial_weight_matrix[i][j] == "random":
+                    initial_weight_matrix[i][j] = random.uniform(-1, 1)
+                elif initial_weight_matrix[i][j] == "VW":  # weak
+                    initial_weight_matrix[i][j] = random.uniform(0, 0.25)
+                elif initial_weight_matrix[i][j] == "W":  # weak
+                    initial_weight_matrix[i][j] = random.uniform(0.1, 0.4)
+                elif initial_weight_matrix[i][j] == "M":  # weak
+                    initial_weight_matrix[i][j] = random.uniform(0.35, 0.65)
+                elif initial_weight_matrix[i][j] == "S":  # strong
+                    initial_weight_matrix[i][j] = random.uniform(0.55, 0.85)
+                elif initial_weight_matrix[i][j] == "VS":  # very strong
+                    initial_weight_matrix[i][j] = random.uniform(0.75, 1)
+
+                elif initial_weight_matrix[i][j] == '"-VW"':  # Minus Very Weak
+                    initial_weight_matrix[i][j] = random.uniform(-0.25, 0)
+                elif initial_weight_matrix[i][j] == '"-W"':  # Minus Weak
+                    initial_weight_matrix[i][j] = random.uniform(-0.4, -0.1)
+                elif initial_weight_matrix[i][j] == '"-M"':  # weak
+                    initial_weight_matrix[i][j] = random.uniform(-0.65, -0.35)
+                elif initial_weight_matrix[i][j] == '"-S"':  # Strong
+                    initial_weight_matrix[i][j] = random.uniform(-0.85, -0.55)
+                elif initial_weight_matrix[i][j] == '"-VS"':  # Minus Very Strong
+                    initial_weight_matrix[i][j] = random.uniform(-1, -0.75)
+        
+
+    else:
+        initial_weight_matrix= np.full((num_dimensions, num_dimensions), "random")
+        
+        for i, row in enumerate(initial_weight_matrix):
+            for j, val in enumerate(row):
+
+                if initial_weight_matrix[i][j] == "random":
+                    initial_weight_matrix[i][j] = random.uniform(-1, 1)
+
+
+
+        np.fill_diagonal(initial_weight_matrix, 0)
+        # Extract output values
+        initial_weight_matrix[-1, :] = 0
+        initial_weight_matrix = np.array(initial_weight_matrix, dtype=float)
+       
+ 
+    return initial_weight_matrix
+
