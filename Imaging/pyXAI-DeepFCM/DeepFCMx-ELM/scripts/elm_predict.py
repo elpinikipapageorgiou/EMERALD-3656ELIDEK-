5d3d3ae3c2a1face7c3a882f59bdfacbc63e129c
Anna Feleki <felekiann@gmail.com>
2026-02-17 21:20:04 +0200

    Delete Imaging/pyXAI-DeepFCM directory

--- Imaging/pyXAI-DeepFCM/DeepFCMx-ELM/scripts/elm_predict.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def elm_predict_function(input_data, input_weights, output_weights):
-    hidden_layer_output = np.dot(input_data, input_weights.T)
-    output_weights = output_weights[:-1]
-    predicted_output = np.dot(hidden_layer_output, output_weights)
-    return predicted_output, hidden_layer_output
